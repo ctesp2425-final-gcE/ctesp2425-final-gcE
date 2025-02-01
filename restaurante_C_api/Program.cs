6046d525fca3d93c48f7a6710f6acca0236b8885
Miguel Ribeiro <m.angeloribeiro04@gmail.com>
2025-02-01 19:40:07 +0000

    Integração do DbContext

--- restaurante_C_api/Program.cs
+++ restaurante_C_api/Program.cs
@@ -6,7 +6,7 @@ using restaurante_C_api.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Configurar string de conexão diretamente no código
-var connectionString = "Server=sql,1440;Database=RestauranteDB;User Id=sa;Password=123G3FJJMA.;Encrypt=True;TrustServerCertificate=True;";
+var connectionString = "Server=127.0.0.1,1440;Database=RestauranteDB;User Id=sa;Password=123G3FJJMA.;Encrypt=True;TrustServerCertificate=True;";
 
 // Registrar o DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
